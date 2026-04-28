db075d3084c889b0776c9b65377f7760fe6c78fc
divyagundeti08 <divyagundeti08@gmail.com>
2026-04-28 08:25:29 +0300

    Add Repository pattern infrastructure for Phase 4

---
+++ Models/DataLayer/IRepository.cs
@@ -0,0 +1,14 @@
+using System.Linq.Expressions;
+
+namespace Group5Flight.Models.DataLayer
+{
+    public interface IRepository<T> where T : class
+    {
+        IEnumerable<T> List(QueryOptions<T> options);
+        T? Get(int id);
+        void Insert(T entity);
+        void Update(T entity);
+        void Delete(T entity);
+        void Save();
+    }
+}
